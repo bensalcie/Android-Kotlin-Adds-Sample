6a4160b8a4ff4a867b524114fb86d4cd2cc0a5be
bensalcie <bensalcie@gmail.com>
2021-07-26 23:50:03 +0300

    Initial commit

---
+++ app/src/main/java/com/modcom/bensalcie/demofirebasechatandads/MainActivity.kt
@@ -0,0 +1,51 @@
+package com.modcom.bensalcie.demofirebasechatandads
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.google.android.gms.ads.*
+import com.google.android.gms.ads.interstitial.InterstitialAd
+import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var mAdView: AdView
+    private lateinit var mInterstitialAd: InterstitialAd
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        mAdView = findViewById(R.id.adView)
+        initializeAds()
+        loadBannerAd()
+        loadInterstitialAd()
+    }
+
+
+    private fun initializeAds() {
+        MobileAds.initialize(this) { }
+
+    }
+
+    private fun loadBannerAd() {
+        val adRequest = AdRequest.Builder().build()
+        mAdView.loadAd(adRequest)
+    }
+    private fun loadInterstitialAd() {
+
+        val adRequest = AdRequest.Builder().build()
+        InterstitialAd.load(this,"ca-app-pub-3940256099942544/1033173712", adRequest, object : InterstitialAdLoadCallback() {
+            override fun onAdFailedToLoad(adError: LoadAdError) {
+                Log.d("inter", adError.message)
+            }
+
+            override fun onAdLoaded(interstitialAd: InterstitialAd) {
+                mInterstitialAd = interstitialAd
+                mInterstitialAd.show(this@MainActivity)
+
+            }
+        })
+    }
+}
