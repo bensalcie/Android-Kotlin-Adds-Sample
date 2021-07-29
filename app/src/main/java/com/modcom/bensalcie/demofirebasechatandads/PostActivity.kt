e38ae937e5d601e426482651126bdffdf6a04a5d
bensalcie <bensalcie@gmail.com>
2021-07-29 19:08:53 +0300

    Fixed Images

--- app/src/main/java/com/modcom/bensalcie/demofirebasechatandads/PostActivity.kt
+++ app/src/main/java/com/modcom/bensalcie/demofirebasechatandads/PostActivity.kt
@@ -56,30 +56,29 @@ class PostActivity : AppCompatActivity() {
                 var imageurl = "default"
             if (imageUri!=null){
                 //image  supplied
+                val uploadTask:UploadTask = storageReference.putFile(imageUri!!)
+                uploadTask.continueWithTask { task ->
+                    if (!task.isSuccessful) {
+                        task.exception?.let {
+                            throw it
+                        }
+                    }
+                    storageReference.downloadUrl
+                }.addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        val downloadUri = task.result
+                        uploadToFirebase(title = title,description= descrition,imageurl = downloadUri.toString())
 
+                    } else {
+                        Toast.makeText(this, "Something went wrong while uploading image", Toast.LENGTH_SHORT).show()
+                    }
+                }
 
             }else{
                 //image not supplied
                 progressBar.visibility = View.VISIBLE
+                uploadToFirebase(title = title,description= descrition,imageurl = imageurl)
 
-                val hashMap = HashMap<String, Any>()
-                hashMap["title"] = title
-                hashMap["description"] = descrition
-                hashMap["timestamp"] = System.currentTimeMillis()
-                hashMap["image"]="default"
-                //random key
-                val postid = databaseReference.push().key.toString()
-
-                databaseReference.child(postid).updateChildren(hashMap).addOnCompleteListener{
-                    if (it.isSuccessful){
-                        progressBar.visibility = View.GONE
-
-                        Toast.makeText(this, "Posted Successfully", Toast.LENGTH_SHORT).show()
-                    }else{
-                        progressBar.visibility = View.GONE
-                        Toast.makeText(this, "Error:${it.exception?.message}", Toast.LENGTH_SHORT).show()
-                    }
-                }
             }
 
 
@@ -87,6 +86,28 @@ class PostActivity : AppCompatActivity() {
         }
 
 
+    }
+//function to upload to firebase
+    //please supply title, description and image
+    private fun uploadToFirebase(title:String, description:String,  imageurl:String){
+        val hashMap = HashMap<String, Any>()
+        hashMap["title"] = title
+        hashMap["description"] = description
+        hashMap["timestamp"] = System.currentTimeMillis()
+        hashMap["image"]=imageurl
+        //random key
+        val postid = databaseReference.push().key.toString()
+        databaseReference.child(postid).updateChildren(hashMap).addOnCompleteListener{
+            if (it.isSuccessful){
+                progressBar.visibility = View.GONE
+                Toast.makeText(this, "Posted Successfully", Toast.LENGTH_SHORT).show()
+                startActivity(Intent(this,MainActivity::class.java))
+                finish()
+                    }else{
+                progressBar.visibility = View.GONE
+                Toast.makeText(this, "Error:${it.exception?.message}", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
